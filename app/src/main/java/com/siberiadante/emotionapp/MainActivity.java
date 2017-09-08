da18953a59154bc14b23134acf7413d048d1a758
SiberiaDante <994537867@qq.com>
2017-09-08 11:07:18 +0800

    add leakcancry test

---
+++ app/src/main/java/com/siberiadante/emotionapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.siberiadante.emotionapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_emotion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditEmotionActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/siberiadante/emotionapp/MyApplication.java
@@ -0,0 +1,24 @@
+package com.siberiadante.emotionapp;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * @Created SiberiaDante
+ * @Describe：
+ * @Time: 2017/9/8
+ * @Email: 994537867@qq.com
+ * @GitHub: https://github.com/SiberiaDante
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
